189b0f488f8e12e667a11053aa803510a275cab7
mariusz498 <hajdylamariusz@gmail.com>
2019-12-19 17:08:47 +0100

    OrderController - wstępna implementacja

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.util.StringUtils;
+
 public class OrderController {
+
+    private List<GenericEntity> orders = new ArrayList<>();
+
+    public List<GenericEntity> getAllOrders() {
+        return orders;
+    }
+
+    public void addOrder(GenericEntity order) {
+        orders.add(order);
+    }
+
+    public String showOrder(Long id) {
+        String result = orders.stream()
+                .filter(o -> o.getId().equals(id))
+                .toString();
+        if(!StringUtils.isEmpty(result)) {
+            return result;
+        }
+        return "Error: Order not found.";
+    }
+
+    public GenericEntity updateOrder(Long id) {
+        return new GenericEntity("Updated order");
+    }
+
+    public void deleteOrder(Long id) {
+
+    }
 }
