4a38af5e333dd303a2f51de48b56f0db025a656f
BartoszekA <agnieszka.bartoszek@gmail.com>
2019-12-20 11:23:29 +0100

    ProductController - wstępna implementacja

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,38 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+
+    private List<GenericEntity> productList = new ArrayList<>();
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProductList")
+    public List<GenericEntity> getProductList() {
+        return productList;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProductById")
+    public GenericEntity getProductById(@RequestParam Long productId) {
+        GenericEntity product = productList.stream()
+                .filter(i -> i.getId().equals(productId)).findAny().get();
+        return product;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createProduct")
+    public GenericEntity createProduct() {
+        return new GenericEntity("New order");
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
+    public void updateProduct() {
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(@RequestParam Long productId){
+    }
 }
